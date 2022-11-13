24d62dec739057963ce712bc0785e31d4a2f5bb6
usefSaeed <youssef.okab@gmail.com.com>
2022-11-13 22:15:09 +0200

    added get(i,j) method in state

--- StateFile.py
+++ StateFile.py
@@ -2,68 +2,72 @@ def pb(num):
     if num >= 1:
         pb(num // 2)
     print(num % 2, end = "")
+COLUMN_COUNT = 7
+ROW_COUNT = 6
 
 class State:
     def __init__(self ,s):
-        self.stateCarrier = s
+        self.value = s
 
     def move(self ,colChosen):
         turn = self.checkTurn()
         requiredBlock = self.getLastColBlock(colChosen)
-        if requiredBlock==6:
+        if requiredBlock == ROW_COUNT:
             print("Invalid Move. Column is full!")
             return
-        self.stateCarrier |= turn << 9*colChosen + requiredBlock
+        self.value |= turn << 9 * colChosen + requiredBlock
         requiredBlock += 1
-        self.stateCarrier &= ~(7 << 6+9*colChosen)
-        self.stateCarrier |= requiredBlock << 6+9*colChosen
+        self.value &= ~(7 << 6 + 9 * colChosen)
+        self.value |= requiredBlock << 6 + 9 * colChosen
         self.changeTurn()
         print("Move Done")
 
-
     def getLastColBlock(self,col):
-        return (self.stateCarrier & 7 << 6 + 9 * col) >> 6 + 9 * col
+        return (self.value & 7 << ROW_COUNT + 9 * col) >> ROW_COUNT + 9 * col
 
     def checkTurn(self):
-        return self.stateCarrier >> 64
+        return self.value >> 64
 
     def changeTurn(self):
-        self.stateCarrier ^= (1 << 64)
-        print("It's Player", self.checkTurn()+1,"'s turn")
-
+        self.value ^= 1 << 64
 
     def mapToState(self,board,turn):
-        self.stateCarrier = 0
+        self.value = 0
         for j in range (7):
             stopped = False
             for temp_i in range (5,-1,-1):
                 i = 5-temp_i
                 if board[temp_i][j]==0:
-                    self.stateCarrier |= i << 6 + 9 * j
+                    self.value |= i << ROW_COUNT + 9 * j
                     stopped = True
                     break
                 if board[temp_i][j]==2:
-                    self.stateCarrier |= 1 << i + 9 * j
+                    self.value |= 1 << i + 9 * j
             if not stopped:
-                self.stateCarrier |= 6 << 6 + 9 * j
+                self.value |= ROW_COUNT << ROW_COUNT + 9 * j
 
-        self.stateCarrier |= (turn << 64)
-        print("Resulted State: ",self.stateCarrier)
+        self.value |= (turn << 64)
+        print("Resulted State: ", self.value)
 
     def showState(self):
-        board = [[0 for i in range(7)] for j in range(6)]
-        for j in range (7):
+        board = [[0 for i in range(COLUMN_COUNT)] for j in range(ROW_COUNT)]
+        for j in range (COLUMN_COUNT):
             checkPoint = self.getLastColBlock(j)
             for temp_i in range (checkPoint):
                 i = 5 - temp_i
-                board[i][j] = ((self.stateCarrier & 1 << (temp_i + 9 * j)) >> (temp_i+9*j)) + 1
+                board[i][j] = ((self.value & 1 << (temp_i + 9 * j)) >> (temp_i + 9 * j)) + 1
         for i in board:
             print('\t'.join(map(str, i)))
         print("It's Player", self.checkTurn()+1,"'s turn")
 
-    def evaluation(self):
-        #TODO
-        pass
+    def get(self,i,j):
+        thres = self.getLastColBlock(j)
+        i = 5-i
+        if (i<thres):
+            return ((self.value & 1 << (i + 9 * j)) >> (i + 9 * j)) + 1
+        return 0
+
+
 
     def getHeuristic(self):
         #TODO
@@ -71,25 +75,21 @@ class State:
 
 
 board1 = [
-        [1, 1, 1, 1, 0, 1, 2],
-        [2, 2, 2, 2, 1, 1, 2],
-        [1, 1, 2, 2, 1, 2, 1],
-        [2, 2, 1, 1, 2, 1, 2],
-        [1, 1, 1, 1, 2, 2, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [2, 2, 2, 2, 0, 1, 2],
+        [1, 1, 2, 2, 0, 2, 1],
+        [2, 2, 1, 1, 0, 1, 2],
+        [1, 1, 1, 1, 0, 2, 1],
         [1, 2, 2, 2, 1, 2, 1],
 ]
 board2 = [
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [2, 1, 1, 0, 0, 0, 0],
+        [2, 2, 1, 1, 0, 0, 0],
+        [2, 1, 2, 2, 1, 0, 0],
 ]
 myState = State(18446744073709551616)
-myState.mapToState(board1,0)
-myState.move(4)
-pb(myState.stateCarrier)
-print()
-print(myState.stateCarrier)
+myState.mapToState(board2,1)
 myState.showState()
