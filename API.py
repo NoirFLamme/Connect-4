698451b4d23281dd287240a78ba32f6a42604bf0
NoirFLamme <ramyfetteha@gmail.com>
2022-11-16 18:07:45 +0200

    fixed wrong mapping

--- API.py
+++ API.py
@@ -1,13 +1,20 @@
+import copy
 import math
+
+import numpy as np
 
 from StateFile import State
 from AIModule import  minimax,minimaxAlphaBeta
 
 
 def getMove(board, algo):
+    ourboard = copy.deepcopy(board)
+    for i in range(int(len(board) / 2)):
+        ourboard[[i, len(board) - 1 - i]] = ourboard[[len(board) - 1 - i, i]]
     state = State()
-    state.mapToState(board, 1)
+    state.mapToState(ourboard, 1)
     if algo == "min":
         return minimax(5, state, True)[1]
     else:
+        # print(minimaxAlphaBeta(5, state, True, -math.inf, math.inf)[1])
         return minimaxAlphaBeta(5, state, True, -math.inf, math.inf)[1]
