6979791c253669441cb2b6de3994cdde7b37aa22
NoirFLamme <ramyfetteha@gmail.com>
2022-11-16 12:07:47 +0200

    alpha beta

--- AI-Module.py
+++ AI-Module.py
@@ -40,34 +40,46 @@ def minimax(depth, state: State, maxplayer:bool):
         return minEval, minEvalIndex
 
 
-def minimaxAlphaBeta(depth, InitialState, maxplayer, alpha, beta):
+def minimaxAlphaBeta(depth, state: State, maxplayer, alpha, beta):
     #TODO
     if depth == 0:
-        return InitialState.getHeuristic()
+        return state.getHeuristic(), 0
     if maxplayer:
-        maxEval = sys.maxsize
-        children = []
+        maxEval = -math.inf
+        maxEvalIndex = -1
+        moves = []
         for i in range(7):
-            children.append(copy.deepcopy(InitialState).move(i))
-        for child in children:
-            eval = minimax(depth - 1, child, False)
-            maxEval = max(maxEval, eval)
+            if state.valid_move(i):
+                child = copy.deepcopy(state)
+                child.move(i)
+                moves.append((child, i))
+        for move in moves:
+            eval = minimax(depth - 1, move[0], False)[0]
+            if eval > maxEval:
+                maxEval = eval
+                maxEvalIndex = move[1]
             alpha = max(alpha, eval)
-            if beta <= alpha:
-                break
-        return maxEval
+            if beta < alpha:
+                return maxEval, maxEvalIndex
+        return maxEval,  maxEvalIndex
     else:
-        minEval = -sys.maxsize - 1
-        children = []
+        minEval = math.inf
+        minEvalIndex = -1
+        moves = []
         for i in range(7):
-            children.append(copy.deepcopy(InitialState).move(i))
-        for child in children:
-            eval = minimax(depth - 1, child, True)
-            maxEval = min(maxEval, eval)
+            if state.valid_move(i):
+                child = copy.deepcopy(state)
+                child.move(i)
+                moves.append((child, i))
+        for move in moves:
+            eval = minimax(depth - 1, move[0], False)[0]
+            if eval < minEval:
+                minEval = eval
+                minEvalIndex = move[1]
             beta = min(beta, eval)
-            if beta <= alpha:
-                break
-        return minEval
+            if beta < alpha:
+                return minEval, minEvalIndex
+        return minEval, minEvalIndex
 
 
 board1 = [
@@ -84,3 +96,4 @@ myState.mapToState(board1, 0)
 myState.move(3)
 depth = int(input())
 print(minimax(depth,myState,True))
+print(minimaxAlphaBeta(depth, myState, True, -math.inf, math.inf))
